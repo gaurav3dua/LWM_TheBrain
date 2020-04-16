8205fb1966b08e28f01374c56f87cfbe77621cf1
Gaurav Dua <gaurav3dua@gmail.com>
2020-04-17 04:09:23 +0530

    LWM-8 Clan Info API

--- helper_main.py
+++ helper_main.py
@@ -1,3 +1,4 @@
+import sys
 import pymongo
 
 import config
@@ -7,39 +8,43 @@ def get_clan_info(content):
 	Provides basic info about all clans
 	"""
 	# print("content:", content)
-	clans = content.get("clans") or []
-	conn_string = config.get_mongo_connection_string()
-	if conn_string is None:
-		return {
-			"conn_string": conn_string,
-			"type": str(type(conn_string)),
-			"message": "mongo conn_string not set"
-		}
-	# print(conn_string)
-	client = pymongo.MongoClient(conn_string)
-	# print("connected to server")
-	db = client['clan']
-	# print("connected to db")
-	col = db['clan_info']
-	# print("connected to collection")
-	if len(clans) == 0:
-		query = {}
-	else:
-		query = {"clan_id": {"$in": clans}}
-	# print("query:", query)
 	try:
-		records = col.find(query).sort("clan_id")
+		clans = content.get("clans", [])
+		conn_string = config.get_mongo_connection_string()
+		if conn_string is None:
+			return {
+				"conn_string": conn_string,
+				"type": str(type(conn_string)),
+				"message": "mongo conn_string not set"
+			}
+		# print(conn_string)
+		client = pymongo.MongoClient(conn_string)
+		# print("connected to server")
+		db = client['clan']
+		# print("connected to db")
+		col = db['clan_info']
+		# print("connected to collection")
+		if len(clans) == 0:
+			query = {}
+		else:
+			query = {"clan_id": {"$in": clans}}
+		# print("query:", query)
+		try:
+			records = col.find(query).sort("clan_id")
+		except:
+			return {
+				"conn_string": conn_string,
+				"type": str(type(conn_string)),
+				"message": "could not fetch from mongo"
+			}
+		# print("records:", records)
+		clan_records = list()
+		for record in records:
+			del record["_id"]
+			# print(record)
+			clan_records.append(record)
+		# print(len(clan_records))
+		return clan_records
 	except:
-		return {
-			"conn_string": conn_string,
-			"type": str(type(conn_string)),
-			"message": "could not fetch from mongo"
-		}
-	# print("records:", records)
-	clan_records = list()
-	for record in records:
-		del record["_id"]
-		# print(record)
-		clan_records.append(record)
-	# print(len(clan_records))
-	return clan_records
+		print "Unexpected error:", sys.exc_info()[0]
+    	raise

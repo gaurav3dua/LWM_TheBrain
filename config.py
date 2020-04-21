0beceaac1533b64c412b9e1606bf1b4dc240c551
Gaurav Dua <gaurav3dua@gmail.com>
2020-04-22 00:23:52 +0530

    LWM-1 added env vars for local development

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+import environment_variables
+
 def get_mongo_connection_string():
-	mongo_connection_string = os.environ.get("MongoConn")
+	mongo_connection_string = os.environ.get("MongoConn", environment_variables.MongoConn)
 	return mongo_connection_string
